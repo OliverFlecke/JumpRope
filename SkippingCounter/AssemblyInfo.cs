33485e75d06718249cc24cd6c233f2d50ec35655
Oliver Fleckenstein <oliverfl@live.dk>
2021-03-27 20:16:57 +0100

    Added icons

--- SkippingCounter/AssemblyInfo.cs
+++ SkippingCounter/AssemblyInfo.cs
@@ -1,6 +1,8 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("ionicons.ttf", Alias = "Ionicons")]
 
 namespace System.Runtime.CompilerServices
 {
---
+++ SkippingCounter/Resources/Icons/Ionicons.cs
@@ -0,0 +1,9 @@
+﻿namespace SkippingCounter.Resources.Icons
+{
+    public static class Ionicons
+    {
+        public const string Body = "\uf188";
+
+        public const string History = "\uf4d0";
+    }
+}
